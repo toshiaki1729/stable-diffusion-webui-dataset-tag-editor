9f6656ff3d3578e139209a3bb0accb56d25396b4
toshiaki1729 <116595002+toshiaki1729@users.noreply.github.com>
2022-12-22 14:01:40 +0900

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
 import launch
 
 if not launch.is_installed("onnxruntime-gpu"):
-    launch.run_pip("install onnxruntime-gpu", "requirements for using SmilingWolf/wd-v1-4-vit-tagger")
+    launch.run_pip("install onnxruntime-gpu", "requirements for dataset-tag-editor [onnxruntime-gpu]")
