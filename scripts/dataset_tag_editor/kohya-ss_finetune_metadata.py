772ddd4798a4554138005ea576256de6abd6f827
toshiaki1729 <116595002+toshiaki1729@users.noreply.github.com>
2023-02-05 03:10:08 +0900

    added license information

--- scripts/dataset_tag_editor/kohya-ss_finetune_metadata.py
+++ scripts/dataset_tag_editor/kohya-ss_finetune_metadata.py
@@ -1,10 +1,17 @@
+# This code is based on following codes written by kohya-ss and modified by toshiaki1729.
+# https://github.com/kohya-ss/sd-scripts/blob/main/finetune/merge_captions_to_metadata.py
+# https://github.com/kohya-ss/sd-scripts/blob/main/finetune/merge_dd_tags_to_metadata.py
+
+# The original code is distributed in the Apache License 2.0.
+# Full text of the license is available at the following link.
+# https://www.apache.org/licenses/LICENSE-2.0
+
+# implement metadata output compatible to kohya-ss's finetuning captions
+# on commit hash: ae33d724793e14f16b4c68bdad79f836c86b1b8e
+
 import json
 from pathlib import Path
 
-# implement metadata output compatible to kohya-ss's finetuning captions
-# https://github.com/kohya-ss/sd-scripts/blob/main/finetune/merge_captions_to_metadata.py
-# https://github.com/kohya-ss/sd-scripts/blob/main/finetune/merge_dd_tags_to_metadata.py
-# on commit hash: ae33d724793e14f16b4c68bdad79f836c86b1b8e
 def write(dataset, dataset_dir, out_path, in_path=None, overwrite=False, save_as_caption=False, use_full_path=False):
     dataset_dir = Path(dataset_dir)
     if in_path is None and Path(out_path).is_file() and not overwrite:
